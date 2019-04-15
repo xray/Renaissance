2a5a40d80cd0d510f0b2e75108b79d3d900a4a3c
xray <creator@darkmattercreations.com>
2019-04-15 09:26:43 -0500

    simplify  return

--- lib/renaissance_web/helpers/auth.ex
+++ lib/renaissance_web/helpers/auth.ex
@@ -3,10 +3,6 @@ defmodule RenaissanceWeb.Helpers.Auth do
   def signed_in?(conn) do
     user_email = Plug.Conn.get_session(conn, :current_user)
 
-    if user_email == nil do
-      false
-    else
-      true
-    end
+    user_email != nil
   end
 end
