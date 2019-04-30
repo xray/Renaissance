61e62c977e1f1af54b24112ad23d6da49f5d29cd
Rachel Stein <rstein66@gmail.com>
2019-04-30 17:42:44 -0500

    Fix test file

--- test/renaissance/auctions/auction_test.exs
+++ test/renaissance/auctions/auction_test.exs
@@ -4,25 +4,26 @@ defmodule Renaissance.Test.AuctionTest do
   require Ecto.Query
   alias Renaissance.Auction
 
-  @now DateTime.utc_now()
-  @after_one_day %{@now | day: @now.day + 1}
+  def fixture(:valid_params) do
+    {:ok, datetime, 0} = DateTime.from_iso8601("2149-04-21T12:00:00+0000")
 
-  @valid_params %{
-    title: "Test title",
-    description: "Test description",
-    seller_id: 1,
-    price: 100,
-    end_auction_at: @after_one_day
-  }
+    %{
+      title: "Test title",
+      description: "Test description",
+      seller_id: 1,
+      price: 100,
+      end_auction_at: datetime
+    }
+  end
 
   describe "user" do
     test "accepts valid params" do
-      changeset = Auction.changeset(%Auction{}, @valid_params)
+      changeset = Auction.changeset(%Auction{}, fixture(:valid_params))
       assert changeset.valid? == true
     end
 
     test "requires title" do
-      invalid_params = Map.put(@valid_params, :title, "")
+      invalid_params = %{fixture(:valid_params) | title: ""}
       changeset = Auction.changeset(%Auction{}, invalid_params)
 
       refute changeset.valid?
@@ -30,15 +31,15 @@ defmodule Renaissance.Test.AuctionTest do
     end
 
     test "requires description" do
-      invalid_params = Map.put(@valid_params, :description, "")
+      invalid_params = %{fixture(:valid_params) | description: ""}
       changeset = Auction.changeset(%Auction{}, invalid_params)
 
       refute changeset.valid?
       assert "can't be blank" in errors_on(changeset).description
     end
 
-    test "requires user id" do
-      invalid_params = Map.put(@valid_params, :seller_id, nil)
+    test "requires seller id" do
+      invalid_params = %{fixture(:valid_params) | seller_id: nil}
       changeset = Auction.changeset(%Auction{}, invalid_params)
 
       refute changeset.valid?
@@ -46,7 +47,7 @@ defmodule Renaissance.Test.AuctionTest do
     end
 
     test "requires price" do
-      invalid_params = Map.put(@valid_params, :price, nil)
+      invalid_params = %{fixture(:valid_params) | price: nil}
       changeset = Auction.changeset(%Auction{}, invalid_params)
 
       refute changeset.valid?
@@ -54,7 +55,7 @@ defmodule Renaissance.Test.AuctionTest do
     end
 
     test "requires price to be greater than 0" do
-      invalid_params = Map.put(@valid_params, :price, 0)
+      invalid_params = %{fixture(:valid_params) | price: 0}
       changeset = Auction.changeset(%Auction{}, invalid_params)
 
       refute changeset.valid?
@@ -62,16 +63,16 @@ defmodule Renaissance.Test.AuctionTest do
     end
 
     test "requires end date" do
-      invalid_params = Map.put(@valid_params, :end_auction_at, nil)
+      invalid_params = %{fixture(:valid_params) | end_auction_at: nil}
       changeset = Auction.changeset(%Auction{}, invalid_params)
 
       refute changeset.valid?
       assert "can't be blank" in errors_on(changeset).end_auction_at
     end
 
-    test "requires future end date" do
-      {:ok, datetime, 0} = DateTime.from_iso8601("2019-01-01T12:00:00+0000")
-      invalid_params = Map.put(@valid_params, :end_auction_at, datetime)
+    test "requires end date to be in the future" do
+      {:ok, past_datetime, 0} = DateTime.from_iso8601("2019-01-01T12:00:00+0000")
+      invalid_params = %{fixture(:valid_params) | end_auction_at: past_datetime}
       changeset = Auction.changeset(%Auction{}, invalid_params)
 
       refute changeset.valid?
